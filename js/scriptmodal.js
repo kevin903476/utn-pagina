aa8714530b11be1eae9d1f24054788af47c09b37
Jonathan20044 <116660564+Jonathan20044@users.noreply.github.com>
2024-06-17 11:49:42 -0600

    modales e imagenes
    se le incorporo ventanas modales a la carrera de iti y tambien se le incorporo imagenes

---
+++ js/scriptmodal.js
@@ -0,0 +1,72 @@
+document.getElementById('openModalBtn').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('modal').style.display = 'block';
+});
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal').style.display = 'none';
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal').style.display = 'none';
+});
+
+
+
+
+document.getElementById('openModalBtn2').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('modal2').style.display = 'block';
+});
+
+document.querySelector('.close-btn2').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal2').style.display = 'none';
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal2').style.display = 'none';
+});
+
+
+
+
+
+document.getElementById('openModalBtn3').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('modal3').style.display = 'block';
+});
+
+document.querySelector('.close-btn3').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal3').style.display = 'none';
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal3').style.display = 'none';
+});
+
+
+
+
+
+document.getElementById('openModalBtn4').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('modal4').style.display = 'block';
+});
+
+document.querySelector('.close-btn4').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal4').style.display = 'none';
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal4').style.display = 'none';
+});
+
+
