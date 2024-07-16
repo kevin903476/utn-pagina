073ba2cf3f9f36820ea6b6ca6855f286a1f210d8
MariaJesus12 <marychuartaviasol2606@gmail.com>
2024-07-16 17:32:08 -0600

    cambios

--- js/controllerSendEmail.js
+++ js/controllerSendEmail.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sendEmailBtn = document.getElementById('send-email-btn');
+    const sendEmailBtn = document.getElementById('#send-email-btn');
   
     sendEmailBtn.addEventListener('click', function () {
-      const email = localStorage.getItem('email'); // Obtén el correo electrónico del localStorage
+      const email = localStorage.getItem('email'); // Obtener correo de localStorage
   
       fetch('https://api-utn.up.railway.app/send-email', {
         method: 'POST',
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .then(response => response.json())
       .then(data => {
-        const messageDiv = document.getElementById('message');
+        const messageDiv = document.getElementById('#message');
         if (data.error) {
           messageDiv.textContent = 'Error al enviar los resultados';
           messageDiv.style.color = 'red';
