a97d513fe9d7929f9dfa2d23af1427c917df7241
MariaJesus12 <marychuartaviasol2606@gmail.com>
2024-07-18 12:08:34 -0600

    prueba envio

--- js/controllerSendEmail.js
+++ js/controllerSendEmail.js
@@ -1,33 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sendEmailBtn = document.getElementById('#send-email-btn');
-  
-    sendEmailBtn.addEventListener('click', function () {
-      const email = localStorage.getItem('email'); // Obtener correo de localStorage
-  
-      fetch('https://api-utn.up.railway.app/send-email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email })
-      })
-      .then(response => response.json())
-      .then(data => {
-        const messageDiv = document.getElementById('#message');
-        if (data.error) {
-          messageDiv.textContent = 'Error al enviar los resultados';
-          messageDiv.style.color = 'red';
-        } else {
-          messageDiv.textContent = 'Resultados enviados exitosamente';
-          messageDiv.style.color = 'green';
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        const messageDiv = document.getElementById('message');
+  const sendEmailBtn = document.getElementById('send-email-btn');
+
+  sendEmailBtn.addEventListener('click', function () {
+    
+    fetch('https://api-utn.up.railway.app/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: localStorage.getItem("email")
+        }) 
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      const messageDiv = document.getElementById('message');
+      if (data.error) {
         messageDiv.textContent = 'Error al enviar los resultados';
         messageDiv.style.color = 'red';
-      });
+      } else {
+        messageDiv.textContent = 'Resultados enviados exitosamente';
+        messageDiv.style.color = 'green';
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      const messageDiv = document.getElementById('message');
+      messageDiv.textContent = 'Error al enviar los resultados';
+      messageDiv.style.color = 'red';
     });
   });
-  
+});
