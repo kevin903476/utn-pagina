4796582f116f4f49b4bf9fb9c26e15af597176f5
CHIKITIN19 <141179159+CHIKITIN19@users.noreply.github.com>
2024-08-02 14:43:46 -0600

    Tablas pdf, creacion de pag energetica
    Se creo la tabla energetica solo a vista, los ejercicios saldran como "ejercicio en construccion" y se agrego los botones para descargar pdf para cada tabla

--- js/PdfScript.js
+++ js/PdfScript.js
@@ -1,23 +1,8 @@
+       
 document.getElementById('download-pdf').addEventListener('click', function () {
-    var doc = new jspdf.jsPDF();
-    html2canvas(document.getElementById('table-promedio')).then(function(canvas) {
-        var imgData = canvas.toDataURL('image/png');
-        var imgWidth = 190; 
-        var pageHeight = 295;  
-        var imgHeight = canvas.height * imgWidth / canvas.width;
-        var heightLeft = imgHeight;
-
-        var position = 10;
-
-        doc.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
-        heightLeft -= pageHeight;
-
-        while (heightLeft >= 0) {
-            position = heightLeft - imgHeight;
-            doc.addPage();
-            doc.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
-            heightLeft -= pageHeight;
-        }
-        doc.save('tabla_promedios.pdf');
-    });
+    var { jsPDF } = window.jspdf;
+    var doc = new jsPDF();
+    doc.autoTable({ html: '#table-promedio' });
+    doc.save('tabla_promedios.pdf');
 });
+
