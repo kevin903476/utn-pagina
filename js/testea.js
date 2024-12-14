3a258d30f8a5f0d7f3da82ac5e883246be861049
kevin903476 <73252529+kevin903476@users.noreply.github.com>
2024-12-13 19:22:37 -0600

    ejercicios corregir
    ejercicios

--- js/testea.js
+++ js/testea.js
@@ -1,7 +1,7 @@
 
 const terminar = document.getElementById("terminar");
 
-let correctas=[1,2,3,1,2,2,3,1,3,3];
+let correctas=[1,3,1,1,3,2,1,2,3,3];
 let opcion_elegida = [];
 let cantidad_correctas = 0;
 
